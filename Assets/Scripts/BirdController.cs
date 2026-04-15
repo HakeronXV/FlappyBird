2b5a13ca6d2d3b471f7b96a9a07ad24f5c13f728
Hakeron <richardyerna@gmail.com>
2026-04-15 16:51:59 +0200

    Ajout de technique cassé FlappyB

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -8,10 +8,10 @@ public class BirdController : MonoBehaviour
     [Range(1f, 10f)]
     public float jumpForce = 5f;
     private Rigidbody2D _rigidbody2D;
-    public enum GameState { Playing, GameOver }
-    public GameState gameState;
+    [SerializeField] private GameManager.GameState _gameManager;
     public TMP_Text gameOverText;
-    
+    private Camera _camera;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Trigger detected ");
@@ -32,36 +32,26 @@ public class BirdController : MonoBehaviour
 
     private void Start()
     {
-        gameState = GameState.Playing;
+        _camera = Camera.main;
         _rigidbody2D = GetComponent<Rigidbody2D>();
         if (_rigidbody2D == null) Debug.LogError("No Rigidbody2D found");
-        Debug.Log("Bird ready");
         
     }
-    
-    void OnCollisionEnter2D(Collision2D collision)
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Obstacle"))
-        {
-            gameState = GameState.GameOver;
-            gameOverText.enabled = true;
-            Debug.Log("Game Over");
-        }
+        if (!collision.gameObject.CompareTag("Obstacle")) return;
+        _gameManager = GameManager.GameState.GameOver;
+        gameOverText.enabled = true;
+        Debug.Log("Game Over");
     }
     
     private void Update()
     { 
         HandleInput();
-        switch (gameState)
+        if(Mathf.Abs(transform.position.y)>_camera.orthographicSize)
         {
-            case GameState.Playing:
-                Time.timeScale = 1f;
-                break;
-            case GameState.GameOver:
-                Time.timeScale = 0f;
-                break;
-            
-            
+            _gameManager = GameManager.GameState.GameOver;
         }
     }
 }
